f7f60ae498cd2c9e273ebf732005c158cf070787
Alexander Gantikow <alexander@gantikow.com>
2017-08-22 14:50:45 +0200

    Solved bug in schema.xml which caused a deletion of text field content while updating. Hence the workaround of reposting the text in 'sort_portfolios' is obsolete.

--- app/controllers/sort_portfolios_controller.rb
+++ app/controllers/sort_portfolios_controller.rb
@@ -1,6 +1,5 @@
 class SortPortfoliosController < ApplicationController
 
-
   def index
   end
 
@@ -15,30 +14,23 @@ class SortPortfoliosController < ApplicationController
 	render :action => 'index'
   end
 
-
   def update
-	@field = params[:field]
+    @field = params[:field] #field to be updated
     @new_content = params[:new_content]
     @checkmarks = params[:index]
-    @new_text = "default"
 
-	# Server is globally set in application rb
-	server = Rails.application.config.solr_server
-	solr = RSolr.connect :url => server
-	@checkmarks.each_with_index do |id, i|
-		@new_text = params[id.to_sym]
-		@new_text = @new_text.gsub('\n', ' ')
-    	@new_text = @new_text.gsub! '\"', '"'
-		@new_text = @new_text[2..-2]
-		field = "#{@field}\:"
-   		#found on https://github.com/rsolr/rsolr/issues/137
-		solr.update(data: [{ :id => id, "#{@field}" => { "set" => @new_content}, :text => { "set" => @new_text}}].to_json, :headers => { 'Content-Type' => 'application/json' })
-	end
-	solr.commit
-	solr.optimize
-	@redirected = true
-	render :action => 'index'
-	#redirect_to action: :index
+    # Server is globally set in application.rb
+    server = Rails.application.config.solr_server
+    solr = RSolr.connect :url => server
+    @checkmarks.each_with_index do |id, i|
+      #found on https://github.com/rsolr/rsolr/issues/137
+      solr.update(data: [{ :id => id, "#{@field}" => { "set" => @new_content}}].to_json, :headers => { 'Content-Type' => 'application/json' })
+    end
+  	solr.commit
+  	solr.optimize
+  	@redirected = true
+  	render :action => 'index'
+  	#redirect_to action: :index
   end
 
 end
